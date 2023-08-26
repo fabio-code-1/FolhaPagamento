a14fcd7dc6f9e2292f147c30d352b7df5772dd63
fabio-code-1 <fabiodesenvolvedor7@gmail.com>
2023-08-26 11:34:30 -0300

    Adicionar arquivos de projeto.

---
+++ FolhaPagamento/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FolhaPagamento.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
